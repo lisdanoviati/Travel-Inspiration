a989fe306a1a62074d11b5ccb0a2dcd1e5ef1599
Kelly Wong <k3llyw9@gmail.com>
2025-05-20 20:26:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ItineraryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/itinerary', [ItineraryController::class, 'index'])->name('itinerary.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
